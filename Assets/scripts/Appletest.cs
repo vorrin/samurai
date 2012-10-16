c4813ace25493cfe6971fe87305cb2a49e851d0c
Vorrin <vorrin.tal@gmail.com>
2012-10-16 18:03:14 +0100

    nustuff

--- Assets/scripts/Appletest.cs
+++ Assets/scripts/Appletest.cs
@@ -63,7 +63,7 @@ public class Appletest : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		Debug.DrawLine(Camera.mainCamera.transform.position,debugVec);
 		ownTime += Time.deltaTime/flyingTime;
 		float curveMoment = curve.Evaluate(ownTime);
 		if (curveMoment >= 1){
@@ -75,18 +75,25 @@ public class Appletest : MonoBehaviour {
 		transform.position = new Vector3(tmpPos.x, height, tmpPos.z);		
 	}
 	
+	private Vector3 debugVec = Vector3.zero;
+	
 	public void Hit(){
 		Debug.Log("GHT");
 		animation.enabled = false;
 		Vector3 baseVector = Camera.mainCamera.transform.forward;
 		//baseVector = new Vector3( Random.Range(-0.7F,0.7F)  ,Random.Range(-0.2F,1.2F) * 1 ,Random.Range(0.7F,1.3F) * 1); 
 		Vector3 worldMousePos = Camera.mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1));
-		Ray ray = new Ray(Camera.mainCamera.transform.position,  new Vector3( worldMousePos.x  ,worldMousePos.y,20));
+		//Ray ray = new Ray(Camera.mainCamera.transform.position,  new Vector3( worldMousePos.x  ,worldMousePos.y,1000));
 		
 //		baseVector = new Vector3( Input.mousePosition.x  ,Input.mousePosition.y ,Random.Range(0.7F,1.3F) * 1); 
-		baseVector = (worldMousePos - Camera.mainCamera.transform.position) * 1000 ;//new Vector3( worldMousePos.x  ,worldMousePos.y,20);
+		baseVector = ( worldMousePos - Camera.mainCamera.transform.position) * 1000 ;//new Vector3( worldMousePos.x  ,worldMousePos.y,20);
 		Debug.Log(baseVector);
+		
+		debugVec = baseVector;
+		
 		rigidbody.AddForce( baseVector);
+		
+		Debug.Log("Camera left and right world space at z = 1.58 " + Camera.mainCamera.ViewportToWorldPoint(new Vector3(0F,0.5F,1.58F)) + " and " + Camera.mainCamera.ViewportToWorldPoint(new Vector3(1F,0.5F,1.58F)));
 		
 	}
 	
